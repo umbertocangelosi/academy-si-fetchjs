689ed93c56a144a54d790a263266ffa532bf38fb
umbertocangelosi <umbertocangelosi97@outlook.it>
2024-05-02 23:24:32 +0200

    added login, registrazione

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ async function creazionetabella() {
 
   const div_utenti = document.createElement("div");
   div_utenti.id = "div_utenti";
-  div_utenti.classList.add("container");
 
   document.body.appendChild(div_utenti);
 
   const tabellaUtenti = document.createElement("table");
+
   tabellaUtenti.classList.add(
     "table",
     "table-striped",
@@ -37,6 +37,7 @@ async function creazionetabella() {
   const rigaIntestazione = intestazioneTabella.insertRow();
 
   const intestazioneNomi = ["Nome", "Cognome", "Email"];
+
   intestazioneNomi.forEach(function (nome) {
     const cellaIntestazione = document.createElement("th");
     cellaIntestazione.textContent = nome;
@@ -55,103 +56,93 @@ async function creazionetabella() {
     cellaCognome.textContent = datiUtente.cognome;
 
     const cellaEmail = riga.insertCell();
-    cellaEmail.id = "elementoInteresse";
     cellaEmail.textContent = datiUtente.email;
 
     const cellaButton = riga.insertCell();
     cellaButton.innerHTML =
-      "<a href='dettagli_utenti.html" +
+      "<a href='dettagli_utente.html" +
       "?email=" +
       datiUtente.email +
-      "'> <button class='btn'>Dettagli utente</button>";
+      "'> <button class='btn btn-primary'>Dettagli utente</button>";
   });
 
   div_utenti.appendChild(tabellaUtenti);
 }
 
 async function loginUtente() {
+  try {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    fetch("http://localhost:8080/api/utente/login-user", {
-      method: "POST",
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({email: email, password: password,}),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        // code here //
-        if (data.error) {
-          alert("Error Password or Username"); 
-        } else {
-          window.open(
-            "target.html"
-          ); 
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-
-async function registrazioneUtente(form) {
-  const formData = new FormData(form);
-  console.log(formData);
-
-  const nome = formData.get("nome");
-  const cognome = formData.get("cognome");
-  const email = formData.get("email");
-  const password = formData.get("password");
-
-  try {
-    const response = await inviaDatiAlServer(nome, cognome, email, password);
-    alert("Dati inviati con successo al server!");
-  } catch (error) {
-    alert(
-      "Si è verificato un errore durante l'invio dei dati al server. Si prega di riprovare più tardi."
+    const response = await fetch(
+      "http://localhost:8080/api/utente/login-user",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      }
     );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      const encodedEmail = encodeURIComponent(email);
+      const profiloUtenteURL = "profilo_utente.html";
+      window.location.href = profiloUtenteURL + "?email=" + encodedEmail;
+    }
+  } catch (e) {
+    console.error("Errore:", error);
+    var loginContainer = document.getElementById("loginInfo");
+    loginContainer.innerHTML = "<p>" + error.message + "</p>";
   }
 }
 
-async function inviaDatiAlServer(nome, cognome, email, password) {
-  await fetch("http://localhost:8080/api/utente/registrazione", {
-    method: "POST", // Metodo HTTP per la richiesta POST
-    headers: {
-      "Content-Type": "application/json", // Imposta l'intestazione del contenuto come JSON
-    },
-    body: JSON.stringify({
-      nome: nome,
-      cognome: cognome,
-      password: password,
-      email: email,
-    }),
-  })
-    .then(async (response) => await response.json()) // Converte la risposta in JSON
-    .then((data) => console.log(data)) // Gestisce i dati della risposta
-    .catch((error) => console.error("Errore:", error)); // Gestisce eventuali errori
-
-  console.log(response);
+async function infoUtente() {
+  try {
+    dettagliUtente();
+  } catch (error) {
+    console.error("Errore:", error);
+    alert("Errore durante il caricamento dei dati utente");
+  }
 }
 
-async function caricaFileEsterno(idElemento) {
+async function registrazioneUtente() {
   try {
-    const response = await fetch("elenco_utenti.html");
-    const html = await response.text();
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, "text/html");
-    const elementoInteresse = doc.getElementById(idElemento);
+    const nome = document.getElementById("nome").value;
+    const cognome = document.getElementById("cognome").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-    return elementoInteresse.textContent;
-  } catch (error) {
-    console.error(
-      "Si è verificato un errore durante il caricamento del file HTML esterno:",
-      error
+    const response = await fetch(
+      "http://localhost:8080/api/utente/registrazione",
+      {
+        method: "POST", // Metodo HTTP per la richiesta POST
+        headers: {
+          "Content-Type": "application/json", // Imposta l'intestazione del contenuto come JSON
+        },
+        body: JSON.stringify({
+          nome: nome,
+          cognome: cognome,
+          password: password,
+          email: email,
+        }),
+      }
     );
+
+    if (!response.ok) {
+      throw new Error("Dati non validi / utente già registrato.");
+    } else {
+      window.location.href = "login.html"; 
+    }
+  } catch (error) {
+    console.error("Errore:", error);
+    var loginContainer = document.getElementById("registerInfo");
+    loginContainer.innerHTML = "<p>" + error.message + "</p>";
   }
 }
 
